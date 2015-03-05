bd3507c29250bd575359f82102d1884a7e0a6559
Krish Furia <krishfuria13@icloud.com>
2015-03-05 11:10:14 -0800

    Initial Commit

---
+++ connect4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  connect4
+//
+//  Created by Krish Furia on 3/5/15.
+//  Copyright (c) 2015 Krish Furia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
